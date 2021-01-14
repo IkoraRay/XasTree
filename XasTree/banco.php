07c8c2f85997f68e6fe74d3faf5d497eb3db5614
IkoraRay <luis.stein@hotmail.com>
2021-01-14 17:58:52 -0300

    Desenvolvimento Concluído

--- XasTree/banco.php
+++ XasTree/banco.php
@@ -1,17 +1,17 @@
 <?php
-
+// Definições de login no banco de dados
 $bdserver = '127.0.0.1';
 $bduser = 'sysclient';
 $bdpassword = 'senha';
 $bdbank = 'xastree';
 
 $conexao = mysqli_connect($bdserver, $bduser, $bdpassword, $bdbank);
-
+//Execução da conexão com o banco
 if (!$conexao){
     echo "Erro de Conexão.";
     die();
 }
-
+//Função com o comando de busca em SQL
 function busca_invest($conexao , $ID){
     $search = "SELECT * FROM prod p, invest i, func f WHERE p.idprod = i.prod_id AND i.func_id = f.id AND f.id = $ID" ;
     $result = mysqli_query($conexao, $search);
@@ -25,7 +25,7 @@ function busca_invest($conexao , $ID){
 
     return $invests;
 }
-
+//Função com o comando de busca em SQL
 function busca_info($conexao , $ID){
     $search = "SELECT fname, lname, email FROM func WHERE func.id = $ID";
     $result = mysqli_query($conexao, $search);
@@ -34,3 +34,4 @@ function busca_info($conexao , $ID){
 
     return $info;
 }
+?>
